59c991b7ad0444d58a5114484e9f9786619803eb
acalcutt <acalcutt@techidiots.net>
2025-12-25 13:13:48 -0500

    Update baseplate_integration.py

--- r2/r2/lib/baseplate_integration.py
+++ r2/r2/lib/baseplate_integration.py
@@ -31,7 +31,7 @@ than its own implementations.
 
 import functools
 
-from baseplate.core import BaseplateObserver, ServerSpanObserver, SpanObserver
+from baseplate import BaseplateObserver, ServerSpanObserver, SpanObserver
 from pylons import app_globals as g
 from pylons import tmpl_context as c
 
