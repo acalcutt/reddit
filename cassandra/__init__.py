de0c73b8e4b87332eedd8f72ecd02f84e48fffbb
acalcutt <acalcutt@techidiots.net>
2025-12-26 16:49:10 -0500

    Update __init__.py

--- cassandra/__init__.py
+++ cassandra/__init__.py
@@ -1,4 +1,18 @@
 class InvalidRequest(Exception):
     """Placeholder for cassandra.InvalidRequest used in compat layer."""
 
-__all__ = ["InvalidRequest"]
+
+class ReadTimeout(Exception):
+    """Placeholder for cassandra.ReadTimeout exception."""
+
+
+class _ConsistencyLevel:
+    ANY = 0
+    ONE = 1
+    QUORUM = 2
+    ALL = 3
+
+
+ConsistencyLevel = _ConsistencyLevel()
+
+__all__ = ["InvalidRequest", "ReadTimeout", "ConsistencyLevel"]
