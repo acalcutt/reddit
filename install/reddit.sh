0175c26e5856596440831a1b3f485acd8008b4b7
acalcutt <acalcutt@techidiots.net>
2025-12-27 16:37:55 -0500

    Update reddit.sh

--- install/reddit.sh
+++ install/reddit.sh
@@ -165,8 +165,9 @@ for repo in activity websockets; do
 done
 
 # Convert i18n Python 2 scripts to Python 3
-if [ -f "$REDDIT_SRC/i18n/gendata.py" ]; then
-    2to3 -w -n "$REDDIT_SRC/i18n/gendata.py" || true
+if [ -d "$REDDIT_SRC/i18n" ]; then
+    # Convert all Python files in i18n directory
+    find "$REDDIT_SRC/i18n" -name "*.py" -exec 2to3 -w -n {} \; || true
 fi
 
 ###############################################################################
